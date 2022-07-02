2c8e279304faffca30bc9b078f2e1dc77fe2d419
raahi <raahi@DESKTOP-9I8OU3J>
2022-07-02 16:42:29 +0500

    First commit

---
+++ src/main/java/com/mycompany/hotelmanagementsystemproject/HotelManagementSystemProject.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.hotelmanagementsystemproject;
+
+/**
+ *
+ * @author raahi
+ */
+public class HotelManagementSystemProject {
+     
+    public static void main(String[] args) {
+        LoginForm run = new LoginForm();
+        run.show();
+    }
+}
